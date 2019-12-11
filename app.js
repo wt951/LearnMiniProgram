82ee70f6583faf74bc5903e8a1b813c5c04dd1b4
wangtong_glb <1209458863@qq.com>
2019-12-11 15:23:23 +0800

    注册小程序和页面

---
+++ app.js
@@ -0,0 +1,48 @@
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    //判断小程序进入场景
+    console.log(options)
+    switch(options.scene){
+      case 1001:
+        break;
+      case 1002:
+        break;
+    }
+    //获取用户信息，并且获取用户信息之后将用户信息传递给服务器
+    wx.getUserInfo({
+      success:function(res){
+        console.log(res)
+      }
+    })
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+  globaData:{
+    name:'codewhy',
+    age:18
+  }
+})
+
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,60 @@
 // pages/home/home.js
+//getapp（）获取app（）产生示例对象
+// const app = getApp()
+// const name = app.globaData.name
+// const age = app.globaData.age
+// console.log(name)
+// console.log(age)
+//注册一个页面
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  //2、初始化数据
+  data:{
+    list:[]
+  },
+  //3、监听wxml中相关的一些事件
+  handleSetUserInfo:function(event){
+    console.log(event)
+  },
+  //1、监听页面的生命周期函数，发送网络请求，从服务器获取数据
+  //页面被加载出来
+  onLoad(){
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success:(res) => {
+    //     console.log(res)
+    //     const data = res.data.data.list 
+    //     this.setData({
+    //       list:data
+    //     })
+    //   }
+    // })
+  },
+  //页面显示出来时
+  onShow(){
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  //页面初次渲染完成时
+  onReady() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //页面隐藏时
+  onHide(){
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  //关于这个页面被销毁时
+  onUnload(){
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //监听页面滚动
+  onPageScroll(obj){
+    console.log(obj)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //监听页面滚动到底部
+  onReachBottom(){
+    console.log('页面滚动到底部')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //监听下拉刷新事件
+  onPullDownRefresh(){
+    console.log('下拉刷新事件')
   }
 })
